096d46da4cb0419d8b8a84632ad3863df93a2176
mateuszawx <59736507+mateuszawx@users.noreply.github.com>
2023-02-23 18:02:38 +0100

    Add files via upload

---
+++ lab01/lab01.py
@@ -0,0 +1,9 @@
+# ZAD 1
+int_liczba1 = 14
+int_liczba2 = 0x1A 
+print('Pierwsza liczba:',int_liczba1)
+print('Druga liczba:',int_liczba2)
+
+float_1 = 3.14
+float_2 = 0.2
+print("Suma:",float_1 + float_2 )
