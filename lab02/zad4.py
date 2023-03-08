9ab32a8117e8811284c0a6faaec1a5b1bcce39f0
mateuszawx <59736507+mateuszawx@users.noreply.github.com>
2023-03-08 18:25:20 +0100

    Update zad4.py

--- lab02/zad4.py
+++ lab02/zad4.py
@@ -1,6 +1,17 @@
+#Datetime
 from datetime import datetime
 dt = datetime(2023, 5, 4, 17, 15)
 print(dt)
 
-print('{:_>20}'.format('test'))
+#Padding
+print('{:_>20}'.format('Padding'))
 
+#Truncating long strings
+print(f'{"przeintelektualizować ":.7}')
+
+#Truncating and align 
+print('{:_<20.7}'.format('przeintelektualizować'))
+
+#Named placeholders
+data = {'first': 'Placki!', 'last': 'Lubie'}
+print('{last} {first}'.format(**data))
